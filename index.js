0d3d58e83ea39a877c92a97c0b17d5bddbd06e37
hyPham-uit <65520566+hyPham-uit@users.noreply.github.com>
2021-10-31 11:24:04 +0700

    add webhook

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port =3000
 
 app.get('/', (req, res)=>{
-    res.send('Hello Jenkins update jenkinsfile2')
+    res.send('Hello Jenkins. Add webhooks')
 })
 
 app.listen(port, () => {

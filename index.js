f847030e43224bb9a4058e45caa4ef3f97682733
hyPham-uit <65520566+hyPham-uit@users.noreply.github.com>
2021-11-06 09:29:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port =3000
 
 app.get('/', (req, res)=>{
-    res.send('Hello Jenkins. Change private webhooks haha')
+    res.send('Hello Jenkins. Test private webhooks SSH')
 })
 
 app.listen(port, () => {

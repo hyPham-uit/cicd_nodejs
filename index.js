de9f7819fbcd283401f7b596f418419654027f9f
hyPham-uit <hypham3012@gmail.com>
2021-12-08 14:23:50 +0700

    test 8-12

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port =3000
 
 app.get('/', (req, res)=>{
-    res.send('Hello Jenkins. Test 8/12/2021')
+    res.send('Hello Jenkins. Test 1 8/12/2021')
 })
 
 app.listen(port, () => {

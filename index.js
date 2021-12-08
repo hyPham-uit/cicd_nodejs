16f8a562d6106b4aea151e80f5141f038856a4d7
hyPham-uit <hypham3012@gmail.com>
2021-12-08 14:21:33 +0700

    test 8-12

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port =3000
 
 app.get('/', (req, res)=>{
-    res.send('Hello Jenkins. Test private webhooks SSH')
+    res.send('Hello Jenkins. Test 8/12/2021')
 })
 
 app.listen(port, () => {
